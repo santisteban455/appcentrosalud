09962b7f44276e59effe46909507938e711ee35d
lilianjudith <lilianocares.05@gmail.com>
2022-06-28 00:05:27 -0500

    Commit 7

---
+++ app/src/main/java/com/example/appcentrosalud/OpiniondeConsultorActivity.java
@@ -0,0 +1,14 @@
+package com.example.appcentrosalud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpiniondeConsultorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opinionde_consultor);
+    }
+}
